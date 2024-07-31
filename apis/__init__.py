a5b917d0b458ab3d361f34af2a0fe329593cccc4
slugb0t <wheresdorian@gmail.com>
2024-07-31 12:13:02 -0700

    add validate endpoint

--- apis/__init__.py
+++ apis/__init__.py
@@ -1,5 +1,7 @@
 """Initialize the api system for the backend"""
 
+import re
+import subprocess
 from flask_restx import Api, Resource
 
 
@@ -20,3 +22,45 @@ class HelloEverynyan(Resource):
         """Returns a simple 'Server Active' message"""
 
         return "Server active!"
+
+
+
+@api.route("/validate", endpoint="validate")
+class ValidateCWL(Resource):
+    """Validate a CWL file"""
+
+    @api.response(200, "Success")
+    @api.response(400, "Validation Error")
+    @api.expect(api.parser().add_argument("file_path", type=str, help="The file path or URL of the CWL file", required=True))
+    def post(self):
+        """Validate a CWL file"""
+
+        file_path = api.payload.get('file_path')
+
+        def clean_output(output):
+            # Remove ANSI escape codes
+            ansi_escape = re.compile(r'\x1B[@-_][0-?]*[ -/]*[@-~]')
+            cleaned_output = ansi_escape.sub('', output).strip()
+            # Remove INFO messages and unnecessary whitespace, and replace newlines with spaces
+            cleaned_lines = []
+            for line in cleaned_output.splitlines():
+                if not line.startswith("INFO"):
+                    cleaned_lines.append(line.strip())
+            return " ".join(cleaned_lines)
+
+        try:
+            result = subprocess.run(
+                ["cwltool", "--validate", file_path],
+                capture_output=True,
+                text=True,
+                check=True,
+            )
+            stdout_clean = clean_output(result.stdout)
+            return {"output": stdout_clean}, 200
+        except subprocess.CalledProcessError as e:
+            stdout_clean = clean_output(e.stdout)
+            return {"output": stdout_clean}, 400
+        except Exception as e:
+            print("An unexpected error occurred.")
+            print(str(e))
+            return {"message": "An unexpected error occurred", "error": str(e)}, 500
